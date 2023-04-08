2aa1a4b29780c7ca74107953354d10153ef4f744
Ori Ofek <oriofek106@gmail.com>
2023-04-09 01:32:11 +0300

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+var value="not logged password";//global variable  
+function myFunction() {
+    if (document.getElementById("username_text").value === "pharoah" && document.getElementById("password_text").value === "' OR 1=1--")
+        {
+            value = "good job your next challenge https://github.com/oo6055/simple-ctf/blob/main/image.jpg";
+            document.getElementById("picture").src = "https://cdn-icons-png.flaticon.com/512/5277/5277377.png";
+
+        }
+            document.getElementById("isloged").innerHTML = value;
+	}
