947b344fe06cd99b194e35dc686534c3f5de7e7d
Ori Ofek <oriofek106@gmail.com>
2023-04-09 02:21:47 +0300

    finish

--- static/js/main.js
+++ static/js/main.js
@@ -5,8 +5,9 @@ function myFunction() {
             value = "good job your next challenge https://github.com/oo6055/passover-ctf-part2/blob/master/riddle.txt";
             document.getElementById("picture").src = "https://tikunhaklali.co.il/wp-content/uploads/2018/02/2.png";
 
-        }
+    }else{
         window.open( 
             "https://www.youtube.com/watch?v=CZgDNPGZ9Sg", "_blank");
-            document.getElementById("isloged").innerHTML = value;
+            document.getElementById("isloged").innerHTML = "wrong username or password";
+    }
 	}
